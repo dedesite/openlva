0ec6b601a7990028c97c436096c2127e1cc06992
Andréas Livet <andreas.livet@gmail.com>
2020-09-07 22:23:34 +0200

    Octave : test fft on wav

--- Octave/wav_fft.m
+++ Octave/wav_fft.m
@@ -11,45 +11,47 @@
 # On ne prend que le channel 1, le 2 ne semble pas utilisé
 audiodata_one = audiodata(:,1);
 
+# Les 5 premières seconde sont les premières données controles
+control_data_1 = audiodata_one(1:end/4);
+# Ensuite c'est la main droite
+right_hand_data = audiodata_one(end/4+1:end/2);
+# Puis la main gauche
+left_hand_data = audiodata_one(end/2+1:end*3/4);
+# Et enfin les deuxièmes données controle
+control_data_2 = audiodata_one(end*3/4+1:end);
+
+# Tentative de soustraction des données controle au données des mains
+clean_right = right_hand_data - control_data_1;
 # 20s à 96000Hz
 step = 1/fs;
-sample_length = length(audiodata_one)*step;
+sample_length = length(right_hand_data)*step;
 t = 0:step:sample_length-step;
 
 subplot(4,1,1)
-plot(t, audiodata_one);
+plot(t, right_hand_data);
+title("Données main droites brutes");
+xlabel('Seconds'); ylabel('Amplitude');
+
+sample_length = length(clean_right)*step;
+t = 0:step:sample_length-step;
+
+subplot(4,1,2)
+plot(t, clean_right);
+title("Données main droites - controle");
 xlabel('Seconds'); ylabel('Amplitude');
 
 #
 # Affichage des composantes spectrales du wav
 #
-spectr = fft(audiodata_one)
 
-fs/length(spectr)
-
-subplot(4,1,2)
-plot(t,abs(spectr))
-xlabel("Frequency")
-ylabel("Signal amplitude")
-
-
-P2 = abs(spectr/sample_length);
-P1 = P2(1:sample_length/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-
-f = fs*(0:(sample_length/2))/sample_length;
-
-subplot(4,1,3)
-plot(f,P1) 
-title('Single-Sided Amplitude Spectrum of wavfile(t)')
-xlabel('f (Hz)')
-ylabel('|P1(f)|')
-%{
-f2 = fs*(0:(sample_length-1))/sample_length;
-
-subplot(4,1,4)
-plot(f2,P2)
-title('Two-Sided Amplitude Spectrum of X(t)')
-xlabel('f (Hz)')
-ylabel('|P2(f)|')
-%}
+L=length(clean_right);        
+NFFT=L;    
+X=fft(clean_right,NFFT);       
+Px=X.*conj(X)/(NFFT*L); %Power of each freq components       
+fVals=fs*(0:NFFT/2-1)/NFFT;  
+subplot(4,1,3);     
+# Test de limit à 845Hz
+plot(fVals,Px(1:NFFT/2),'b'); axis([0 845 0 0.0000001]);
+title('One Sided Power Spectral Density');       
+xlabel('Frequency (Hz)')         
+ylabel('PSD');
