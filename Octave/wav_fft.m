9716a370a42c41edde4f61e2db5485484d3cf383
Andréas Livet <andreas.livet@gmail.com>
2020-09-08 12:19:24 +0200

    Diff FFT values instead of wav values

--- Octave/wav_fft.m
+++ Octave/wav_fft.m
@@ -6,7 +6,7 @@
 #
 # Affichage du contenu du wav
 #
-[audiodata, fs] = wavread("andreas.wav");
+[audiodata, fs] = audioread("andreas.wav");
 # On ne prend que le channel 1, le 2 ne semble pas utilisé
 audiodata_one = audiodata(:,1);
 
@@ -26,7 +26,10 @@ step = 1/fs;
 sample_length = length(right_hand_data)*step;
 t = 0:step:sample_length-step;
 
-subplot(4,1,1)
+# Put window in fullscreen
+# https://stackoverflow.com/a/45985834/947242
+figure(1,"position",get(0,"screensize"))
+subplot(5,1,1)
 plot(t, right_hand_data);
 title("Données main droites brutes");
 xlabel('Seconds'); ylabel('Amplitude');
@@ -34,7 +37,7 @@ xlabel('Seconds'); ylabel('Amplitude');
 sample_length = length(clean_right)*step;
 t = 0:step:sample_length-step;
 
-subplot(4,1,2)
+subplot(5,1,2)
 plot(t, clean_right);
 title("Données main droites - controle");
 xlabel('Seconds'); ylabel('Amplitude');
@@ -42,11 +45,38 @@ xlabel('Seconds'); ylabel('Amplitude');
 #
 # Affichage des composantes spectrales du wav
 #
-  
-[Px, fVals, NFFT] = fft_one_sided(clean_right, fs);
-subplot(4,1,3);     
-# Test de limit à 845Hz
-plot(fVals,Px(1:NFFT/2),'b'); axis([0 845 0 0.0000001]);
-title('One Sided Power Spectral Density');       
-xlabel('Frequency (Hz)')         
-ylabel('PSD');
+
+[control_pow_1, fVals, NFFT] = fft_one_sided(control_data_1, fs);
+[control_pow_2, fVals, NFFT] = fft_one_sided(control_data_2, fs);
+[right_hand_pow, fVals, NFFT] = fft_one_sided(right_hand_data, fs);
+
+diff_pow = right_hand_pow - control_pow_1;
+
+subplot(5,1,3);
+# Test de limit à 2000Hz
+plot(fVals,diff_pow,'b'); axis([0 2000 -0.00000001 0.00000001]);
+title('Diff. de densité spectrale entre données controle 1 et main droite');
+xlabel('Frequency (Hz)');
+ylabel('PSD Diff');
+
+# Calculate mean of control data power
+control_pow_mean = (control_pow_1 + control_pow_2) ./ 2;
+diff_pow = right_hand_pow - control_pow_mean;
+
+subplot(5,1,4);
+# Test de limit à 2000Hz
+plot(fVals,diff_pow,'b'); axis([0 2000 -0.00000001 0.00000001]);
+title('Diff. de densité spectrale entre moyenne données controle et main droite');
+xlabel('Frequency (Hz)');
+ylabel('PSD Diff');
+
+
+# Tentative d'utilisation d'echelle logarithmique
+subplot(5,1,5);
+# Test de limit à 2000Hz
+semilogx(fVals,diff_pow,'b');
+# bug : xlim ne fonctionne pas avec mes données
+# xlim([0 2000]);
+title('Diff. avec echelle logaritmique');
+xlabel('Frequency (Hz) logarithmic scale');
+ylabel('PSD Diff');
